1ecd714c2781a079c08ab93981ffca388d732b50
Juliana Nguyen <juliana_nguyen@uri.edu>
2024-03-19 00:54:58 -0400

    midterm edition done

--- project/js/picAnimation.js
+++ project/js/picAnimation.js
@@ -1,6 +1,7 @@
-$("img").hide();
-
 $(document).ready(function() {
+    //hide the pic to start
+    $("img").hide();
+
     // Fade in the element over 2 seconds (2000 milliseconds)
     $("img").fadeIn(2000); 
 });
