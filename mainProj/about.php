eea26ad64f6900d4203a7bd8359274baa98d0b86
Juliana Nguyen <juliana_nguyen@uri.edu>
2024-04-29 18:45:57 -0400

    clean

--- mainProj/about.php
+++ mainProj/about.php
@@ -34,7 +34,7 @@
     <div>
         
         <div class="book-layout">
-            <div id="team" style="margin-left: 3%;">
+            <div id="team" style="margin-left: 5%;">
                 <img id="roseHead-img" src="images/roseHead.jpg" alt="Rosemary Nguyen">
                 
                 <img id="andyHead-img" src="images/andyHead.jpg" alt="Andy Nguyen">
