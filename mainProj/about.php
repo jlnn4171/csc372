819e48b6acb34e082ce240644218a027005f9577
Juliana Nguyen <juliana_nguyen@uri.edu>
2024-04-29 18:42:33 -0400

    clean

--- mainProj/about.php
+++ mainProj/about.php
@@ -31,7 +31,7 @@
         <h1 class="titles">About Perfect Nails </h1>
     </div>
     
-    <div class = "containerG">
+    <div>
         <div class="book-layout">
             <div id="details">
                 <h3>Click on a team member from the left to learn more!</h3>
